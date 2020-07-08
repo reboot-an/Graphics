1eb81cce9c421f05ae44232f536e14238ce22b6d
Liis Kivistik <liis.kivistik@unity3d.com>
2020-07-08 09:54:09 +0200

    editor pinning initial copied

--- .yamato/ruamel/jobs/editor/yml_editor.py
+++ .yamato/ruamel/jobs/editor/yml_editor.py
@@ -1,11 +1,14 @@
 
-from .editor import Editor_PrimingJob
-from ..shared.namer import editor_filepath
+from .editor_priming import Editor_PrimingJob
+from .editor_pinning_merge import Editor_PinningMergeJob
+from .editor_pinning_update import Editor_PinningUpdateJob
+from ..shared.namer import editor_filepath, editor_pinning_filepath
 
 def create_editor_yml(metafile):
 
     yml_files = {}
 
+    # editor priming jobs
     yml = {}
     for platform in metafile["platforms"]:
         for editor in metafile['editors']:
@@ -13,4 +16,17 @@ def create_editor_yml(metafile):
             yml[job.job_id] = job.yml
 
     yml_files[editor_filepath()] = yml
+
+
+    # editor pinning jobs
+    yml = {}
+    job = Editor_PinningUpdateJob(metafile["editor_pin_agent"], metafile["editor_pin_target_branch"], metafile["editor_pin_ci_branch"])
+    yml[job.job_id] = job.yml
+
+    job = Editor_PinningMergeJob(metafile["editor_pin_track"], metafile["editor_pin_agent"], metafile["editor_pin_target_branch"], metafile["editor_pin_ci_branch"])
+    yml[job.job_id] = job.yml 
+
+    yml_files[editor_pinning_filepath()] = yml
+
+
     return yml_files
